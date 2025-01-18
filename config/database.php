a84798f1e1dfeb956ddd01dcd0f8660bf72d8eee
LunaPedro17 <paluna@espol.edu.ec>
2025-01-17 19:02:44 -0500

    cambios al backend

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ class Database {
     private $host = "localhost";
     private $user = "root";
     private $password = "";
-    private $dbname = "basedenuncias";
+    private $dbname = "denuncia";
     public $conn;
 
     public function connect() {
